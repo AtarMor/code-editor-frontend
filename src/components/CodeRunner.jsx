acceae354b99646836a07c651e457f12af131fd4
Atar Mor <atarmor92@gmail.com>
2024-05-23 15:42:51 +0300

    Refactor code runner loader

--- src/components/CodeRunner.jsx
+++ src/components/CodeRunner.jsx
@@ -1,11 +1,11 @@
 export function CodeRunner({ output, isError, isRunnerLoading }) {
-    if (isRunnerLoading) return <div className="loader"></div>
     return (
         <div className="code-runner">
+            {isRunnerLoading ? <div className="loader"></div> :
             <div style={isError ? { color: 'red' } : {}}>
                 {output ? output.map((line, idx) => <p key={idx}>{line}</p>)
                     : 'Click "Run" to see the output.'}
-            </div>
+            </div>}
         </div>
     )
 }
